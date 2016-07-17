fdf8f9d7bf8969a4acb7e53c115db7d8c825a4bd
f1lander <=>
2016-07-17 02:39:02 -0600

    adding gitignore

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ var flight = require('./routes/flight');
 
 var app = express();
 
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -26,15 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/flight', flight);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
-// error handlers
 
 // development error handler
 // will print stacktrace
